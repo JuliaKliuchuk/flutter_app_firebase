f4a569cdcce7d2880deaf2b9fda3faceb0aa96ac
Julia Kliuchuk <yulya.kliuchuk@gmail.com>
2022-11-06 01:13:18 +0300

    init firebase remote config

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,56 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      home: FutureBuilder<FirebaseRemoteConfig>(
+        future: setipRemouteConfig(),
+        builder: ((BuildContext context,
+            AsyncSnapshot<FirebaseRemoteConfig> snapshot) {
+          return snapshot.hasData
+              ? Home(remoteConfig: snapshot.requireData)
+              : Container(child: const Text('NOT DATA'));
+        }),
+      ),
+    ),
+  );
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class Home extends AnimatedWidget {
+  final FirebaseRemoteConfig remoteConfig;
 
-  final String title;
+  const Home({super.key, required this.remoteConfig})
+      : super(listenable: remoteConfig);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              '22',
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Remoute config'),
+      ),
+      body: Column(
+        children: [
+          Image.network(remoteConfig.getString('Image')),
+          const Text('test'),
+        ],
       ),
     );
   }
+}
+
+Future<FirebaseRemoteConfig> setipRemouteConfig() async {
+  final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetch();
+  await remoteConfig.activate();
+
+  print(remoteConfig.getString(''));
+
+  return remoteConfig;
 }
