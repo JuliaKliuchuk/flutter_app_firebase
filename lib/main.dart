e38959a5650564c1fbae3e67b7dc7104e4140472
Julia Kliuchuk <yulya.kliuchuk@gmail.com>
2022-11-06 15:18:48 +0300

    added save path through shared_preferences;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,23 +14,42 @@ void main() async {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       home: FutureBuilder<FirebaseRemoteConfig>(
-        future: setipRemouteConfig(),
+        future: setupRemouteConfig(),
         builder: ((BuildContext context,
             AsyncSnapshot<FirebaseRemoteConfig> snapshot) {
           return snapshot.hasData
-              ? Home(remoteConfig: snapshot.requireData)
-              : Container(child: const Text('NOT DATA'));
+              ? HomePage(remoteConfig: snapshot.requireData)
+              : const Center(
+                  child: Text('NOT DATA'),
+                );
         }),
       ),
     ),
   );
 }
 
-class Home extends AnimatedWidget {
+class HomePage extends StatefulWidget {
   final FirebaseRemoteConfig remoteConfig;
 
-  const Home({super.key, required this.remoteConfig})
-      : super(listenable: remoteConfig);
+  const HomePage({Key? key, required this.remoteConfig}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late SharedPreferences _sharedPrefs;
+  static const String _pathSharedPrefsKey = 'path_pref';
+  String _path = '';
+
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((pref) {
+      setState(() => {_sharedPrefs = pref});
+      _checkPath();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,21 +58,61 @@ class Home extends AnimatedWidget {
         title: const Text('Remoute config'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Image.network(remoteConfig.getString('Image')),
-          const Text('test'),
+          // Image.network(widget.remoteConfig.getString('Image')),
+          Text(widget.remoteConfig.getString('url')),
+          Text('_path --- $_path'),
+          FloatingActionButton(
+            onPressed: (() async {
+              try {
+                _resetDataPref();
+                // await widget.remoteConfig.setConfigSettings(
+                //     RemoteConfigSettings(
+                //         fetchTimeout: const Duration(seconds: 10),
+                //         minimumFetchInterval: Duration.zero));
+                // await widget.remoteConfig.fetchAndActivate();
+              } catch (e) {}
+            }),
+            child: const Icon(Icons.refresh),
+          ),
         ],
       ),
     );
   }
+
+// проверка path
+  _checkPath() {
+    _path = _sharedPrefs.getString(_pathSharedPrefsKey) ?? '';
+
+    if (_path.isEmpty) {
+      _loadPath();
+    } else {
+      return _path;
+    }
+  }
+
+// загрузка path
+  void _loadPath() {
+    setState(() {
+      final String path = widget.remoteConfig.getString('url');
+      _sharedPrefs.setString(_pathSharedPrefsKey, path);
+    });
+  }
+
+  // сбросить path
+  Future<void> _resetDataPref() async {
+    await _sharedPrefs.remove(_pathSharedPrefsKey);
+    setState(() {
+      _path = '';
+    });
+  }
 }
 
-Future<FirebaseRemoteConfig> setipRemouteConfig() async {
+Future<FirebaseRemoteConfig> setupRemouteConfig() async {
   final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
   await remoteConfig.fetch();
   await remoteConfig.activate();
-
-  print(remoteConfig.getString(''));
 
   return remoteConfig;
 }
